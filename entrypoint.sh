1a1d466f4d1291a4b9a0007dc01e636a110ee4b6
Leonardo Melo <121765108+ileomelo@users.noreply.github.com>
2024-02-12 12:30:01 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 poetry run alembic upgrade head
 
 # Inicia a aplicação
-poetry run uvicorn --host 0.0.0.0 --port 8000 fast_zero.app:app
+poetry run uvicorn --host 0.0.0.0 --port 8000 fast_hero.app:app
